c2267ba2f4de3ebdb2a49364083c463abea88666
elafarga10 <elafarga10@gmail.com>
2020-09-03 16:54:02 -0700

    temp and location succesfully displayed

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let temp = document.querySelector(".weather__temp");
-
+let icon = document.querySelector(".weather__icon--display");
+let city = document.querySelector(".weather__location");
 
 
 
 
 function getWeather() {
-    let url = "https://api.weatherapi.com/v1/current.json?key=588781a2bffb4d22871224158200309&q=Los Angeles";
+    let url = "https://api.weatherapi.com/v1/current.json?key=588781a2bffb4d22871224158200309&q=London";
 
     let values;
 
@@ -13,11 +14,12 @@ function getWeather() {
     .then((resp) => resp.json())
     .then((resp) => {
         console.log(resp);
-        temp.innerHTML = resp.current.temp_f;
+        temp.innerHTML = `${resp.current.temp_f} F`;
+        icon.src = resp.current.condition.icon;
+        city.innerHTML = resp.location.name;
+        console.log(icon.src);
     })
     .catch(error => console.log(error))
-
-    console.log(values);
 }
 
 getWeather();
