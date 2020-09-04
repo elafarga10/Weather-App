4c6e8cf84df4b1d9b622e15ea7bf941c2c0da8fe
elafarga10 <elafarga10@gmail.com>
2020-09-03 20:23:30 -0700

    added timzone and text for weather

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-let temp = document.querySelector(".weather__temp");
-let icon = document.querySelector(".weather__icon--display");
-let city = document.querySelector(".weather__location");
+window.addEventListener("load", getWeather => {
+    let icon = document.querySelector('.weather__icon--display');
+    let temp = document.querySelector('.weather__temp');
+    let city = document.querySelector('.weather__location');
+    let timezone = document.querySelector('.weather__timezone');
+    let description = document.querySelector('.weather__description')
+    let lat;
+    let long;
+    navigator.geolocation.getCurrentPosition(position => {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        let url = `https://api.weatherapi.com/v1/current.json?key=588781a2bffb4d22871224158200309&q=${lat},${long}`;
 
+        fetch(url)
+        .then((response) => response.json())
+        .then(data => {
+            console.log(data);
+            description.innerHTML = data.current.condition.text;
+            timezone.innerHTML = data.location.tz_id;
+            icon.src = data.current.condition.icon;
+            temp.innerHTML = data.current.temp_f;
+            city.innerHTML = data.location.name;
+        })
 
-
-
-function getWeather() {
-    let url = "https://api.weatherapi.com/v1/current.json?key=588781a2bffb4d22871224158200309&q=London";
-
-    let values;
-
-    let data = fetch(url)
-    .then((resp) => resp.json())
-    .then((resp) => {
-        console.log(resp);
-        temp.innerHTML = `${resp.current.temp_f} F`;
-        icon.src = resp.current.condition.icon;
-        city.innerHTML = resp.location.name;
-        console.log(icon.src);
     })
-    .catch(error => console.log(error))
-}
-
-getWeather();
+})
